f0cccaac7bfafceea2e2c7d3040d6fcf8be15baf
talattar1234 <attar.tal@gmail.com>
2018-10-26 12:33:01 +0300

    dev

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import './style.scss';
 function Hi(){
 
 };
+
+let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
 console.log(a.b);
---
+++ webpack.common.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+          title: 'Production'
+      }),
+      new CleanWebpackPlugin("./dist/*"),
+      new MiniCssExtractPlugin({
+        filename: "style.[contenthash].css",
+      })
+  ],
+  module: {
+    rules: [
+        {
+            test: /\.css$/,
+            use: [
+               /* 'style-loader',*/
+               MiniCssExtractPlugin.loader,
+                'css-loader'
+            ]
+        },
+        {
+            test: /\.scss$/,
+            use: [
+                /*"style-loader", // creates style nodes from JS strings*/
+                MiniCssExtractPlugin.loader,
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+        },
+        {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+            }
+          }
+         ]
+       },
+};
