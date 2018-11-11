a0496dea64bf45a7ad355f009aff23c013478f15
talattar1234 <attar.tal@gmail.com>
2018-11-11 23:59:44 +0200

    dev

--- src/_test.js
+++ src/_test.js
@@ -4,10 +4,11 @@ import {sendAjaxRequest} from './ajaxCaller';
 export const test = () => {
     sendAjaxRequest({retryNumber: 10,axiosParams: {
         method: "get",
-        urtl: 'https://httpbin.org/get',
-    }}).then(()=>{
-        alert("1").catch(()=>{
+        url: 'https://httpb3in.org/get',
+        timeout: 3000
+    }}).then((data)=>{
+        alert(JSON.stringify(data))}).catch(()=>{
             alert("fucking ajax error")
-        });
+        
     })
 }
