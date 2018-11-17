9280c41f6ce67676a6fe6f7e1b295143d8dcaf47
talattar1234 <attar.tal@gmail.com>
2018-11-17 11:01:31 +0200

    responsive

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {TextField, Typography, FormHelperText} from '@material-ui/core';
 import {withStyles} from '@material-ui/core/styles';
 import {LTR} from '../../direction';
+import withWidth from '@material-ui/core/withWidth';
+import compose from 'recompose/compose';
 const styles = (theme) => ({
     root: {
         
@@ -9,13 +11,31 @@ const styles = (theme) => ({
     },
     myRaw: {
         display: "flex"
-    }
+    },
+
+    ele1: {
+        color: "pink",
+        [theme.breakpoints.between('sm', 'md')]: {
+            backgroundColor: 'red',
+        },
+        [theme.breakpoints.up('md')]: {
+            backgroundColor: 'blue',
+          },
+    },
+   
+
   
 });
 
 class Dashboard extends React.Component {
+    
     render(){
-        const {root,myRaw} = this.props.classes;
+
+        const {root,myRaw,ele1} = this.props.classes;
+        const {width} = this.props;/* width: 'sm' \ 'xl' \ ... */
+        
+        
+
         return (
             <React.Fragment>
                 <Typography variant="h3">
@@ -24,9 +44,19 @@ class Dashboard extends React.Component {
                     <LTR>
                         <TextField  margin="normal" type="text" label="name" />
                     </LTR>
+
+                    <div className = {ele1}>
+                        This is responsive element - {width}
+                    </div>
+
+                    
             </React.Fragment>
         );
     }
 
 }
-export default withStyles(styles)(Dashboard);
+//export default withStyles(styles)(Dashboard);
+export default compose(
+    withStyles(styles),
+    withWidth()
+)(Dashboard);
