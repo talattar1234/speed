5de37808ab0e399766aac1ebb27f8450f76fba60
talattar1234 <attar.tal@gmail.com>
2019-06-01 12:15:07 +0300

    update packages to latest

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
@@ -15,7 +15,7 @@ module.exports = {
           template: "src/index.hbs",
           description: "this is description"
       }),
-      new CleanWebpackPlugin("./dist/*"),
+      new CleanWebpackPlugin(),
       new MiniCssExtractPlugin({
         filename: "style.[contenthash].css",
       }),
