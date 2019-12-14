2c234b94619f3426b5f610ebce486a86f00a7daa
talattar1234 <attar.tal@gmail.com>
2019-12-14 09:24:06 +0200

    hooks

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {TextField, Typography, FormHelperText} from '@material-ui/core';
-import {withStyles} from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import {LTR} from '../../direction';
-import withWidth from '@material-ui/core/withWidth';
-import compose from 'recompose/compose';
-const styles = (theme) => ({
+import useWidth from '../../customHooks/useWidth';
+
+//import compose from 'recompose/compose';
+
+const useStyles = makeStyles((theme) => ({
     root: {
         
         /*flip: false*/
@@ -22,20 +24,13 @@ const styles = (theme) => ({
             backgroundColor: 'blue',
           },
     },
-   
+}));
 
-  
-});
-
-class Dashboard extends React.Component {
-    
-    render(){
-
-        const {root,myRaw,ele1} = this.props.classes;
-        const {width} = this.props;/* width: 'sm' \ 'xl' \ ... */
+const Dashboard = () => {
+        const classes = useStyles();
+        const {root,myRaw,ele1} = classes;
+        const width = useWidth();/* width: 'sm' \ 'xl' \ ... */
         
-        
-
         return (
             <React.Fragment>
                 <Typography variant="h3">
@@ -52,11 +47,13 @@ class Dashboard extends React.Component {
                     
             </React.Fragment>
         );
-    }
-
 }
+
+export default Dashboard;
 //export default withStyles(styles)(Dashboard);
+/*
 export default compose(
     withStyles(styles),
     withWidth()
 )(Dashboard);
+*/
